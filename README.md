a95e0a748b52440030a46b76b8ec8dc8b8a12f7a
建召 <jianzhao@jianzhao.local>
2019-02-27 15:25:27 +0800

    代码部分改善

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # program-language-learn
-本人瞎搞的一个备份，大佬勿喷，谢谢合<br>
-this is a rep about my c  learning<br>
+###作者随便练手的
 ####c语言
 
 地址：https://github.com/jianzhao0806/program-language-learn/tree/master/c<br>
--- python/spider.py
+++ python/spider.py
@@ -1,38 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-def spider_1():
-    url = 'http://www.ygdy8.net/'      #初始url
+import re
+def geturl():       #get电影地址和名称
+    url = 'http://www.dytt8.net/'      #初始url
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)'      #伪装headers
     headers = {'User-Agent':user_agent}
     response = requests.get(url,headers=headers)
     soup = BeautifulSoup(response.content,'html.parser',from_encoding='gb18030')        #soup解析
     titles = soup.find_all('td')        #寻找td标签
-    list = []                           #创建列表以存储电影详细的URL
-    for title in titles:
-        for n in title.find_all('a'):
-            if '201902'and'gndy'and'dyzz' in str(n):
-                if '2' in n.get('href'):
-                    a.append('http://www.ygdy8.net'+n.get('href'))
-            print('runing spider_1')
-    return list
-def spider_2(urls):
-    global names
-    names = []
-    useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)'
-    headers = {'User-Agent':useragent}
-    for url in urls:
-        response = requests.get(url,headers=headers)
-        soup = BeautifulSoup(response.content,'html.parser',from_encoding='gb18030')
-        head = soup.head.title.string
-        names.append('电影名称: '+ head)
-        print('runing spider_2')
-def spider_3(urls):
+    global list_name,list_url           #定义全局变量存储电影地址和名称
+    list_url = []                       #创建列表以存储电影详细的URL
+    list_name = []                      #存储电影名称
+    for title in titles:                                #提取所需的网址和名称
+        for n in title.find_all('a', href=re.compile('^/html/gndy/dyzz/201')):
+            list_url.append('https://www.dytt8.net'+n.get('href'))
+            list_name.append(n.string)
+def get_dlurl():     #get下载链接
     global downloads
     downloads = []
     useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)'
     headers = {'User-Agent':useragent}
-    for url in urls:
+    for url in list_url:
         response = requests.get(url,headers=headers)
         soup = BeautifulSoup(response.content,'html.parser',from_encoding='gb18030')
         dlurls = soup.find_all('a')
@@ -41,13 +30,14 @@ def spider_3(urls):
             if 'magnet' in str(dlurl):
                 list.append(dlurl)
                 downloads.append('种子链接： '+dlurl.get('href'))
-                print('runing spider_3')
+                print('getting download url')
 def main():
-    spider_2(spider_1())
-    spider_3(spider_1())
-    for (name,download) in zip(names,downloads):
+    geturl()
+    get_dlurl()
+    for (name,download) in zip(list_name,downloads):
         with open('result.txt','a+') as file:
-            file.write(name+'\n'+download + '\n')
-            time.sleep(2)
-    print('Done')
+            file.write('电影名称: '+name+'\n'+download + '\n')
+        time.sleep(1)
+        print('writing the file')
+    print('Done, please open the result.txt.')
 main()
